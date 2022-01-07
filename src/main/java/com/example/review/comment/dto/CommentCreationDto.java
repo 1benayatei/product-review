6e0fa14a13aad11fc69166216ccd51a6692ff7f2
Bahman <1benayatei@gmail.com>
2022-01-08 00:00:19 +0330

    fix(comment): add length validation for CommentCreationDto

--- src/main/java/com/example/review/comment/dto/CommentCreationDto.java
+++ src/main/java/com/example/review/comment/dto/CommentCreationDto.java
@@ -2,6 +2,7 @@ package com.example.review.comment.dto;
 
 import com.example.review.comment.Comment;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
 
@@ -17,6 +18,7 @@ public class CommentCreationDto {
     private Long userId;
 
     @NotNull
+    @Length(min = 10, max = 255)
     private String text;
 
     public Comment toComment() {
